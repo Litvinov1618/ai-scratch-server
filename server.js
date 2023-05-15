159586cd246c6ce3772d545254d668068fc137ee
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-05-15 18:23:47 +0300

    Add search by cosine similarity

--- server.js
+++ server.js
@@ -2,10 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const pool = require('./db');
+const {
+    Configuration,
+    OpenAIApi
+} = require('openai');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const COSINE_SIMILARITY_THRESHOLD = 0.8;
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const createEmbedding = async (text) => {
+    const result = await openai.createEmbedding({
+        model: "text-embedding-ada-002",
+        input: text,
+    })
+
+
+    return result.data.data[0].embedding;
+}
+
+//  TODO: 
+// - Add date calculating
+// - Fix date format (returning as string)
 
 // Routes
 
@@ -15,16 +40,49 @@ app.post('/notes', async (req, res) => {
         const {
             text,
             date,
-            embedding
         } = req.body;
 
+        const embedding = await createEmbedding(text);
+
         const newNote = await pool.query(
-            'INSERT INTO notes (text, date, embedding) VALUES($1, $2, $3) RETURNING *',
-            [text, date, embedding]
+            'INSERT INTO notes (embedding, text, date) VALUES($1::numeric[], $2, $3) RETURNING *',
+            [embedding, text, date]
         );
 
         res.json(newNote.rows[0]);
     } catch (err) {
+        res.json({
+            error: err.message
+        });
+        console.error(err.message);
+    }
+});
+
+// Search notes by query
+app.get('/notes/search', async (req, res) => {
+    try {
+        const { searchValue } = req.query;
+
+        if (!searchValue) {
+            return res.json({
+                error: 'No search value provided'
+            });
+        }
+
+        const embedding = await createEmbedding(searchValue);
+
+        const result = await pool.query(
+            `SELECT 1 - (embedding <=> $1::numeric[]::vector(1536)) AS cosine_similarity, text, date, id 
+            FROM notes 
+            WHERE 1 - (embedding <=> $1::numeric[]::vector(1536)) >= $2;`,
+            [embedding, COSINE_SIMILARITY_THRESHOLD]
+        );
+
+        res.json(result.rows);
+    } catch (err) {
+        res.json({
+            error: err.message
+        });
         console.error(err.message);
     }
 });
@@ -35,6 +93,9 @@ app.get('/notes', async (req, res) => {
         const allNotes = await pool.query('SELECT * FROM notes');
         res.json(allNotes.rows);
     } catch (err) {
+        res.json({
+            error: err.message
+        });
         console.error(err.message);
     }
 });
@@ -48,16 +109,20 @@ app.put('/notes/:id', async (req, res) => {
         const {
             text,
             date,
-            embedding
         } = req.body;
 
+        const embedding = await createEmbedding(text);
+
         const updatedNotes = await pool.query(
-            'UPDATE notes SET text = $1, date = $2, embedding = $3 WHERE id = $4 RETURNING *',
+            'UPDATE notes SET text = $1, date = $2, embedding = $3::numeric[]::vector(1536) WHERE id = $4 RETURNING *',
             [text, date, embedding, id]
         );
 
         res.json(updatedNotes.rows);
     } catch (err) {
+        res.json({
+            error: err.message
+        });
         console.error(err.message);
     }
 });
@@ -71,6 +136,9 @@ app.delete('/notes/:id', async (req, res) => {
         const restOfNotes = await pool.query('DELETE FROM notes WHERE id = $1 RETURNING *', [id]);
         res.json(restOfNotes.rows);
     } catch (err) {
+        res.json({
+            error: err.message
+        });
         console.error(err.message);
     }
 });
