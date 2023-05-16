cc0fa28f2207fbea825c90e685862ae04e3b65eb
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-05-16 18:39:26 +0300

    Add OpenAI env variable

--- index.js
+++ index.js
@@ -27,109 +27,109 @@ app.use(express.json());
 const COSINE_SIMILARITY_THRESHOLD = 0.8;
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
 });
-// const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
-// const createEmbedding = async (text) => {
-//     const result = await openai.createEmbedding({
-//         model: "text-embedding-ada-002",
-//         input: text,
-//     })
+const createEmbedding = async (text) => {
+    const result = await openai.createEmbedding({
+        model: "text-embedding-ada-002",
+        input: text,
+    })
 
 
-//     return result.data.data[0].embedding;
-// }
+    return result.data.data[0].embedding;
+}
 
 // Routes
 
 // Set new note
-// app.post('/notes', async (req, res) => {
-//     try {
-//         const {
-//             text,
-//             date,
-//         } = req.body;
+app.post('/notes', async (req, res) => {
+    try {
+        const {
+            text,
+            date,
+        } = req.body;
 
-//         const embedding = await createEmbedding(text);
+        const embedding = await createEmbedding(text);
 
-//         const newNote = await pool.query(
-//             'INSERT INTO notes (embedding, text, date) VALUES($1::numeric[], $2, $3) RETURNING *',
-//             [embedding, text, date]
-//         );
+        const newNote = await pool.query(
+            'INSERT INTO notes (embedding, text, date) VALUES($1::numeric[], $2, $3) RETURNING *',
+            [embedding, text, date]
+        );
 
-//         res.json(newNote.rows[0]);
-//     } catch (err) {
-//         res.json({
-//             error: err.message
-//         });
-//         console.error(err.message);
-//     }
-// });
+        res.json(newNote.rows[0]);
+    } catch (err) {
+        res.json({
+            error: err.message
+        });
+        console.error(err.message);
+    }
+});
 
 // Search notes by query
-// app.get('/notes/search', async (req, res) => {
-//     try {
-//         const { searchValue } = req.query;
+app.get('/notes/search', async (req, res) => {
+    try {
+        const { searchValue } = req.query;
 
-//         if (!searchValue) {
-//             return res.json({
-//                 error: 'No search value provided'
-//             });
-//         }
+        if (!searchValue) {
+            return res.json({
+                error: 'No search value provided'
+            });
+        }
 
-//         const embedding = await createEmbedding(searchValue);
+        const embedding = await createEmbedding(searchValue);
 
-//         const result = await pool.query(
-//             `SELECT text, date, id 
-//             FROM notes 
-//             WHERE 1 - (embedding <=> $1::numeric[]::vector(1536)) >= $2;`,
-//             [embedding, COSINE_SIMILARITY_THRESHOLD]
-//         );
+        const result = await pool.query(
+            `SELECT text, date, id 
+            FROM notes 
+            WHERE 1 - (embedding <=> $1::numeric[]::vector(1536)) >= $2;`,
+            [embedding, COSINE_SIMILARITY_THRESHOLD]
+        );
 
-//         const completion = await openai.createChatCompletion({
-//             model: "gpt-3.5-turbo",
-//             temperature: 0.7,
-//             messages: [
-//                 {
-//                     role: "system",
-//                     content:
-//                         "You need to give a human-like answer for search request based on given notes. If notes doesn't includes right answer or not enough information, say 'I don't know'. Use only 1-5 sentences in answer.",
-//                 },
-//                 {
-//                     role: "user",
-//                     content: `Question: Adam's birthday? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: Mom's birthday 6th may 2. Date: 9 Oct 2023, 2:10 PM Text: Happy Birthday to you song need to download 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
-//                 },
-//                 {
-//                     role: "assistant",
-//                     content: "Adam's birthday is on 4th July",
-//                 },
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 0.7,
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You need to give a human-like answer for search request based on given notes. If notes doesn't includes right answer or not enough information, say 'I don't know'. Use only 1-5 sentences in answer.",
+                },
+                {
+                    role: "user",
+                    content: `Question: Adam's birthday? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: Mom's birthday 6th may 2. Date: 9 Oct 2023, 2:10 PM Text: Happy Birthday to you song need to download 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
+                },
+                {
+                    role: "assistant",
+                    content: "Adam's birthday is on 4th July",
+                },
 
-//                 {
-//                     role: "user",
-//                     content: `Question: Where I was 9th October? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: "Dark Knight" is amazing movie! 2. Date: 9 May 2023, 2:10 PM Text: Bacca coffee is amazing place, need to return soon 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
-//                 },
-//                 {
-//                     role: "assistant",
-//                     content: "You were in cinema watching 'Dark Knight' movie",
-//                 },
-//                 {
-//                     role: "user",
-//                     content: `Question: ${searchValue} Notes: ${result.rows
-//                         .map((note, index) => `${index + 1}. Date: ${formatDate(+note.date)} Text: ${note.text}`)
-//                         .join(" ")}`,
-//                 },
-//             ],
-//         });
+                {
+                    role: "user",
+                    content: `Question: Where I was 9th October? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: "Dark Knight" is amazing movie! 2. Date: 9 May 2023, 2:10 PM Text: Bacca coffee is amazing place, need to return soon 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
+                },
+                {
+                    role: "assistant",
+                    content: "You were in cinema watching 'Dark Knight' movie",
+                },
+                {
+                    role: "user",
+                    content: `Question: ${searchValue} Notes: ${result.rows
+                        .map((note, index) => `${index + 1}. Date: ${formatDate(+note.date)} Text: ${note.text}`)
+                        .join(" ")}`,
+                },
+            ],
+        });
 
-//         res.json({ posts: result.rows, aiResponse: completion.data.choices[0].message?.content });
-//     } catch (err) {
-//         res.json({
-//             error: err.message
-//         });
-//         console.error(err.message);
-//     }
-// });
+        res.json({ posts: result.rows, aiResponse: completion.data.choices[0].message?.content });
+    } catch (err) {
+        res.json({
+            error: err.message
+        });
+        console.error(err.message);
+    }
+});
 
 // Get all notes
 app.get('/notes', async (req, res) => {
@@ -145,31 +145,31 @@ app.get('/notes', async (req, res) => {
 });
 
 // Alter note
-// app.put('/notes/:id', async (req, res) => {
-//     try {
-//         const {
-//             id
-//         } = req.params;
-//         const {
-//             text,
-//             date,
-//         } = req.body;
+app.put('/notes/:id', async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        const {
+            text,
+            date,
+        } = req.body;
 
-//         const embedding = await createEmbedding(text);
+        const embedding = await createEmbedding(text);
 
-//         const updatedNotes = await pool.query(
-//             'UPDATE notes SET text = $1, date = $2, embedding = $3::numeric[]::vector(1536) WHERE id = $4 RETURNING *',
-//             [text, date, embedding, id]
-//         );
+        const updatedNotes = await pool.query(
+            'UPDATE notes SET text = $1, date = $2, embedding = $3::numeric[]::vector(1536) WHERE id = $4 RETURNING *',
+            [text, date, embedding, id]
+        );
 
-//         res.json(updatedNotes.rows);
-//     } catch (err) {
-//         res.json({
-//             error: err.message
-//         });
-//         console.error(err.message);
-//     }
-// });
+        res.json(updatedNotes.rows);
+    } catch (err) {
+        res.json({
+            error: err.message
+        });
+        console.error(err.message);
+    }
+});
 
 // Delete note
 app.delete('/notes/:id', async (req, res) => {
