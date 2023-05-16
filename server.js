a91d24e8c7cc120e7b27acc6af6699aa90423a52
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-05-16 15:37:42 +0300

    Add OpenAI response to notes search

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const {
     OpenAIApi
 } = require('openai');
 
+const formatDate = (date) => {
+    const d = new Date(date);
+    const day = d.getDate();
+    const month = d.toLocaleString("default", { month: "short" });
+    const year = d.getFullYear();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${day} ${month} ${year}, ${formattedHours}:${formattedMinutes} ${ampm}`;
+};
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -72,13 +85,48 @@ app.get('/notes/search', async (req, res) => {
         const embedding = await createEmbedding(searchValue);
 
         const result = await pool.query(
-            `SELECT 1 - (embedding <=> $1::numeric[]::vector(1536)) AS cosine_similarity, text, date, id 
+            `SELECT text, date, id 
             FROM notes 
             WHERE 1 - (embedding <=> $1::numeric[]::vector(1536)) >= $2;`,
             [embedding, COSINE_SIMILARITY_THRESHOLD]
         );
 
-        res.json(result.rows);
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 0.7,
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You need to give a human-like answer for search request based on given notes. If notes doesn't includes right answer or not enough information, say 'I don't know'. Use only 1-5 sentences in answer.",
+                },
+                {
+                    role: "user",
+                    content: `Question: Adam's birthday? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: Mom's birthday 6th may 2. Date: 9 Oct 2023, 2:10 PM Text: Happy Birthday to you song need to download 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
+                },
+                {
+                    role: "assistant",
+                    content: "Adam's birthday is on 4th July",
+                },
+
+                {
+                    role: "user",
+                    content: `Question: Where I was 9th October? Notes: 1. Date: 9 Oct 2023, 2:00 PM Text: "Dark Knight" is amazing movie! 2. Date: 9 May 2023, 2:10 PM Text: Bacca coffee is amazing place, need to return soon 3. Date: 2 Oct 2023, 2:00 PM Text: Adam's Birthday: 04/07/1990`,
+                },
+                {
+                    role: "assistant",
+                    content: "You were in cinema watching 'Dark Knight' movie",
+                },
+                {
+                    role: "user",
+                    content: `Question: ${searchValue} Notes: ${result.rows
+                        .map((note, index) => `${index + 1}. Date: ${formatDate(+note.date)} Text: ${note.text}`)
+                        .join(" ")}`,
+                },
+            ],
+        });
+
+        res.json({ posts: result.rows, aiResponse: completion.data.choices[0].message?.content });
     } catch (err) {
         res.json({
             error: err.message
