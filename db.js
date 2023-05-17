60b6bce72759391ad2939c824eaf6d10fcee53f4
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-05-17 12:26:19 +0300

    Replace hardcoded variables with environment ones

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 const Pool = require('pg').Pool;
 
-// TODO: replace with POSTGRES_URL?
-// POSTGRES_URL + '?sslmode=require'
 const pool = new Pool({
-    connectionString: "postgres://default:OEQw6y0nhBWG@ep-twilight-wave-073761-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+    connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 module.exports = pool;
