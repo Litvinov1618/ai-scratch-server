dc90b2cb160d0c9e841577f868cfc039ada8ab8a
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-06-16 16:08:55 +0300

    Fix initial db file

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE notes(
 
 CREATE TABLE user_settings (
   notes_similarity_threshold FLOAT CHECK (notes_similarity_threshold >= 0 AND notes_similarity_threshold <= 1),
-  ai_response_temperature_range FLOAT CHECK (ai_response_temperature_range >= 0 AND ai_response_temperature_range <= 1),
+  ai_response_temperature FLOAT CHECK (ai_response_temperature >= 0 AND ai_response_temperature <= 1),
   show_ai_response BOOLEAN,
   email VARCHAR(255) UNIQUE
 );
